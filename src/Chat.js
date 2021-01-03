5b556af3ac0e056cb526752f72e013bda0dbba03
nabeeltaariq <nabeeltaariq@gmail.com>
2021-01-03 12:15:37 -0800

    final for this little project

--- src/Chat.js
+++ src/Chat.js
@@ -5,8 +5,19 @@ import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
 import MicIcon from '@material-ui/icons/Mic'
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
 import './Chat.css'
+import axios from './axios'
 
 function Chat({ messages }) {
+  const [input, setInput] = useState('')
+  const sendmessage = (e) => {
+    e.preventDefault()
+
+    axios.post('/api/v1/messages/new', {
+      name: 'Bhatti',
+      message: input,
+      received: false,
+    })
+  }
   return (
     <div className='chat'>
       <div className='chat__header'>
@@ -45,8 +56,15 @@ function Chat({ messages }) {
       <div className='chat__footer'>
         <InsertEmoticonIcon />
         <form>
-          <input type='text' placeholder='Type a message' />
-          <button type='submit'>Send a Message</button>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type='text'
+            placeholder='Type a message'
+          />
+          <button onClick={sendmessage} type='submit'>
+            Send a Message
+          </button>
         </form>
         <MicIcon />
       </div>
